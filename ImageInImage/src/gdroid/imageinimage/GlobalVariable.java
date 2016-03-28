2eb94369052fe6ebb3dc9aeaa7e17935426eb319
huynhbinh <huynhbinh0511@gmail.com>
2016-03-28 15:27:30 +0700

    binh first commit

---
+++ ImageInImage/src/gdroid/imageinimage/GlobalVariable.java
@@ -0,0 +1,15 @@
+package gdroid.imageinimage;
+
+import android.app.Application;
+import android.graphics.Bitmap;
+
+public class GlobalVariable extends Application
+{
+	
+	Bitmap backGroundBmp ;
+	Bitmap humanBmp;
+	
+	float displayWidth = 480;
+	float displayHeight = 320;
+	
+}
